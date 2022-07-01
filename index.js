db7d129bf8a8cad831024dd829f091f21b23477d
abirisl <mhabir979292@gmail.com>
2022-07-01 10:57:30 +0600

    data connected

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ehowy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function  run(){
+    try{
+         await client.connect();
+         const todoCollection = client.db("todoApp").collection("todos")
+         app.get('/todos' , async(req,res)=>{
+              const query = {}
+              const cursor = todoCollection.find(query)
+              const todos = await cursor.toArray()
+              res.send(todos)
+         })
+       
+    }
+    finally{
+         //await client.close();
+    
+    }
+    }
+    run().catch(console.dir);
+    
+    
+    app.get('/', (req, res) => {
+      res.send('Todo server is runnig')
+    })
+    
+    app.listen(port, () => {
+      console.log('listenig to the port ' , port)
+    })
